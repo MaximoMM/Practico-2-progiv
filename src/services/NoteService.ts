9a9270eb8832e0c35e329cb0b542e7289fdc5c0a
derosatiago-art <derosatiago@gmail.com>
2026-09-23 23:39:49 -0300

    "test: ejercicio 5 (Verde)"

--- src/services/NoteService.ts
+++ src/services/NoteService.ts
@@ -46,7 +46,6 @@ export class NoteServiceImpl implements NoteService {
   }
 
   deleteNote(id: number): boolean {
-    // 🔴🟢 EJERCICIO 5: ciclo completo.
-    throw new Error('deleteNote: no implementado (Ejercicio 5)');
+    return this.noteRepository.delete(id);
   }
 }
